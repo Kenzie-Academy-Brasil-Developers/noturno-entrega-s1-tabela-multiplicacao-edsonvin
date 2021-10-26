bdc451323604331ef46e7814bfedbc0e17a2d53d
edson vinicius <edsonvin1801@gmail.com>
2021-10-25 21:08:03 -0300

    adiciona segundo for + variável result

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,13 +1,30 @@
 function criaArrays(n) { 
-    // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let Numeros = newArray ()
-    numeros[0] = 
-    let x = []
-        for (let contador = 0; contador <= 10; contador++) {
-        x[contador] = [];
-x.length = n +1
-    console.table(x)
+    let numeros = []        //cria o array vazio    
+    for(contador = 0;contador <= n; contador++){        //cria primeiro loop
+        numeros.push([])        //adiciona primeiro contador ao array
+        for (contador2 = 0; contador2 <= n; contador2++){   //cria segundo loop
+            result = contador * contador2
+            numeros[contador].push(result)      //adiciona segundo contador(2) ao array    
+        }
+    }    
+    console.table(numeros)
 }
-}
-criaArrays(9)
+criaArrays(10)
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+//         for (let contador = 0; contador <= 10; contador++) {
+//         x[contador] = [];
+// x.length = n +1
+//     console.table(x)
